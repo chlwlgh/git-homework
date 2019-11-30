bd6b56f534ad7bf9e25c33d76123e18b0e66b6ab
chlwlgh <chlwlgh0502@hansung.ac.kr>
2019-11-30 15:12:37 +0900

    B

--- GitCommandSequense.java
+++ GitCommandSequense.java
@@ -1,6 +1,7 @@
 public class GitCommandSequense {
-   public static void main(String[] args) {
-      System.out.println("A");
+    public static void main(String[] args) {
+        System.out.println("A");
+        System.out.println("B");
    }
 }
 
